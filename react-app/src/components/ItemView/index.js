79956f899f8aa45979dde3ea67486c4b909c45d7
Quintin Hull <quintinhull92@gmail.com>
2021-03-19 19:24:03 -0400

    item view and item create styled

--- react-app/src/components/ItemView/index.js
+++ react-app/src/components/ItemView/index.js
@@ -5,6 +5,8 @@ import ItemCreate from "../ItemCreate";
 // import EditItemView from "../EditItemView";
 import { NavLink, useParams } from "react-router-dom";
 
+import "./ItemView.css"
+
 const ItemView = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
@@ -16,20 +18,39 @@ const ItemView = () => {
     dispatch(getTypeItems(id));
   }, [dispatch, id]);
 
+  const imagePath = process.env.NODE_ENV === "production" ? "/static" : "";
+
   return (
-    <div className="body">
-      {typeItems &&
-        Object.values(typeItems).map((item) => (
-          <NavLink key={item.id} to={`/item/${item.id}`}>
-            <div>{item.id}</div>
-            <div>{item.name}</div>
-            <div>{item.description}</div>
-            <div>
-              {item.first_name} {item.last_name}
-            </div>
-          </NavLink>
-        ))}
-      <div>
+    <div className="item_container">
+      <div className="item_row1">
+        <div className="item_col1">
+          <div className="item_image">
+            <img src={`${imagePath}/carousel_${id}.svg`} alt="type image"></img>
+          </div>
+        </div>
+        <div className="item_col2">
+          <div className="item_content">
+            {typeItems &&
+              Object.values(typeItems).map((item) => (
+                <div className="item_card" key={item.id}>
+                  <div><span className="item_span">item: </span>{item.name}</div>
+                  <div><span className="item_span">tip: </span>{item.description}</div>
+                  <div className="item_bottom_row">
+                    <div>
+                      <span className="item_span">author: </span>
+                      {item.first_name} {item.last_name}
+                    </div>
+                    <NavLink className="type_link" key={item.id} to={`/item/${item.id}`}>
+                      edit
+                    </NavLink>
+                  </div>  
+                  <hr></hr>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+      <div className="item_row2">
         <ItemCreate />
       </div>
     </div>
